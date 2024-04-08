77403b788703c37bc563791f258744095e0c8fab
Jami <jami25265@gmail.com>
2024-04-08 12:28:09 +0600

    Login and Registration view files added

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/log', function () {
+    return view('auth.login');
+});
+
+Route::get('/reg', function () {
+    return view('auth.register');
+});
